44e1d58d9b81d1bc1e261ce2078017d6f33e9fe8
Bryce Murray <brycepmurray711@gmail.com>
2017-11-27 15:39:54 -0700

    commented out Song Router due to only having one playlist anyways

--- server/routes/song-routes.js
+++ server/routes/song-routes.js
@@ -1,21 +1,21 @@
- var Songs = require('..models/Song')
+//  var Songs = require('..models/Song')
 
- router.post('/songs', (req, res, next) => {
-         Songs.create(req.body)
-             .then(post => {
-                 res.send(post)
-             })
-             .catch(err => {
-                 res.status(400).send({ Error: err })
-             })
-     }),
-     router.get('/songs', (req, res, next) => {
-         Songs.find({})
-             .then((songs) => {
-                 res.send(songs)
-             })
-             .catch(err => {
-                 res.status(400).send({ Error: err })
-             })
-     })
- module.exports = router
+//  router.post('/songs', (req, res, next) => {
+//          Songs.create(req.body)
+//              .then(post => {
+//                  res.send(post)
+//              })
+//              .catch(err => {
+//                  res.status(400).send({ Error: err })
+//              })
+//      }),
+//      router.get('/songs', (req, res, next) => {
+//          Songs.find({})
+//              .then((songs) => {
+//                  res.send(songs)
+//              })
+//              .catch(err => {
+//                  res.status(400).send({ Error: err })
+//              })
+//      })
+//  module.exports = router
